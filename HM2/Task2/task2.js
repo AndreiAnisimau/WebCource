ac2060201fc9a4285317929a5bbbd354769d9034
AndreiAnisimau <47625348+AndreiAnisimau@users.noreply.github.com>
2019-05-06 09:26:59 +0300

    Add files via upload

---
+++ HM2/Task2/task2.js
@@ -0,0 +1,9 @@
+document.getElementById("redirectBtn").onclick = function redirect( ) 
+{
+    location.href = "https://www.google.com";
+};
+
+document.getElementById("clearBtn").onclick = function clearall( )
+{
+    window.document.body.innerHTML = "";
+};
